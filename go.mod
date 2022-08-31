f1d7f97461face390a15530ef8ff33d917d45b24
John Jermanis <jermanis@yahoo.com>
2022-08-31 10:01:51 -0700

    Implement base UI

---
+++ go.mod
@@ -0,0 +1,3 @@
+module mymodule.mod
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	PlayGame()
+}
+
+func PlayGame() {
+	fmt.Println("Welcome to peaks-solver-go. This program will help you solve Wordle Peaks puzzles.")
+	fmt.Println("To use, guess what this program suggests. Then, let this program know the result.")
+	fmt.Println("Enter the five colors from the result. G for green, B for blue, and O for orange.")
+
+	var currGuess, result string
+
+	for i := 0; i < 6; i++ {
+		// TODO implement the logic got best guess
+		currGuess = "MINOR"
+		fmt.Println("Please guess:", currGuess)
+		result = PromptResult()
+		if result == "GGGGG" {
+			fmt.Println("Congrats!")
+			return
+		}
+	}
+	fmt.Println("Looks like you ran out of guesses. My fault.")
+}
+
+func PromptResult() string {
+	var result string
+	for {
+		fmt.Print("Result? ")
+		fmt.Scan(&result)
+		result = strings.ToUpper(result)
+		if !IsValidResult(result) {
+			fmt.Println("Incorrect format. Results should be five letters long. G for green, B for blue, and O for orange.")
+		} else {
+			return result
+		}
+	}
+}
+
+func IsValidResult(r string) bool {
+	var VALID_RESULT_CHARS string = "GBO"
+	if len(r) != 5 {
+		return false
+	} else {
+		for i := 0; i < 5; i++ {
+			loc := strings.Index(VALID_RESULT_CHARS, string(r[i]))
+			if loc < 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
